81c57d52d9ffd61376e96b8bc67b1d7b82928d61
Jonathan Felipe <jow@LAPTOP-5OUMU9B7.localdomain>
2021-04-06 14:21:30 -0300

    Correct config for styled components and SSR

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,14 @@
-import Document from 'next/document';
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
@@ -26,5 +32,17 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="pt-BR">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
